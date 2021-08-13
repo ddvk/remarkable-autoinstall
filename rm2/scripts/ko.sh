3860732379e0e1db0d8f2df4cdbb3fa15a7141d8
Nathaniel van Diepen <Eeems@users.noreply.github.com>
2021-08-13 09:39:34 -0600

    Update ko.sh

--- rm2/scripts/ko.sh
+++ rm2/scripts/ko.sh
@@ -1,23 +1,14 @@
 #!/bin/sh
 
-kill_server(){
-  if [ -f /tmp/rm2fb-server.pid ];then
-    pid=$(cat /tmp/rm2fb-server.pid)
-    if kill -s 0 $pid;then
-      kill $pid
-    fi
-    rm /tmp/rm2fb-server.pid
-  fi
-}
-
 if [ -z "$NO_XO" ]; then
     systemctl stop xochitl
 fi
 
-kill_server
+if [ ! -z "`pidof rm2fb-server`"]; then
+    killall rm2fb-server
+fi
 
 LD_PRELOAD=/home/root/librm2fb_server.so.1.0.1 /usr/bin/xochitl &
-echo $! > /tmp/rm2fb-server.pid
 sleep 2
 
 export KO_DONT_SET_DEPTH=1
@@ -26,7 +17,7 @@ export LD_PRELOAD=/home/root/librm2fb_client.so.1.0.1
 
 /home/root/apps/koreader/koreader.sh
 
-kill_server
+killall rm2fb-server
 
 systemctl reset-failed xochitl
 if [ -z "$NO_XO" ]; then
