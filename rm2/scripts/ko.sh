27ab9ad042268c48405cf771dcfd01d4ad8b2ea2
Nathaniel van Diepen <Eeems@users.noreply.github.com>
2021-08-13 09:27:56 -0600

    Update ko.sh

--- rm2/scripts/ko.sh
+++ rm2/scripts/ko.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
+kill-server(){
+  if [ -f /tmp/rm2fb-server.pid ];then
+    pid=$(cat /tmp/rm2fb-server.pid)
+    if kill -s 0 $pid;then
+      kill $pid
+    fi
+    rm /tmp/rm2fb-server.pid
+  fi
+}
+
 if [ -z "$NO_XO" ]; then
     systemctl stop xochitl
 fi
 
-if [ ! -z "`pidof remarkable-shutdown`"]; then
-    killall remarkable-shutdown
-fi
+kill-server
 
 LD_PRELOAD=/home/root/librm2fb_server.so.1.0.1 /usr/bin/xochitl &
+echo $! > /tmp/rm2fb-server.pid
 sleep 2
 
 export KO_DONT_SET_DEPTH=1
@@ -17,7 +26,8 @@ export LD_PRELOAD=/home/root/librm2fb_client.so.1.0.1
 
 /home/root/apps/koreader/koreader.sh
 
-killall remarkable-shutdown
+kill-server
+
 systemctl reset-failed xochitl
 if [ -z "$NO_XO" ]; then
     systemctl start xochitl
