0f0aa4910ee7f485e24cf341296b18d80df9bce5
Nathaniel van Diepen <Eeems@users.noreply.github.com>
2022-11-19 12:33:15 -0700

    Update auto_rm2.sh

--- rm2/auto_rm2.sh
+++ rm2/auto_rm2.sh
@@ -1,8 +1,8 @@
 set -e
 APPDIR=${APPDIR:-/home/root/apps}
 REPOURL="https://raw.githubusercontent.com/ddvk/remarkable-autoinstall/master/rm2"
-RM2FBREPO="https://github.com/ddvk/remarkable2-framebuffer/releases/download/v0.0.26"
-KOREADER="https://github.com/koreader/koreader/releases/download/v2021.10.1/koreader-remarkable-v2021.10.1.zip"
+RM2FBREPO="https://github.com/ddvk/remarkable2-framebuffer/releases/download/v0.0.28"
+KOREADER="https://github.com/koreader/koreader/releases/download/v2022.10/koreader-remarkable-v2022.10.zip"
 
 mkdir -p $APPDIR
 mkdir -p ~/scripts
